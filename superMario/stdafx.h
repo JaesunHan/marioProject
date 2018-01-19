8d5462d584caf6f12247eb0b404b26015c7dd065
DESKTOP-D7ID7GB\HAN <aka8880@gmail.com>
2018-01-19 17:52:58 +0900

    no message

--- superMario/stdafx.h
+++ superMario/stdafx.h
@@ -35,7 +35,7 @@ using namespace HEPTA_UTIL;
 //=======================================================
 
 #define WINNAME		(LPTSTR)TEXT("Hepta Window API")
-#define WINSTARTX	2000	//윈도우 시작좌표 X
+#define WINSTARTX	50		//윈도우 시작좌표 X
 #define WINSTARTY	50		//윈도우 시작좌표 Y
 #define WINSIZEX	800		//윈도우 가로크기
 #define WINSIZEY	800		//윈도우 세로크기
